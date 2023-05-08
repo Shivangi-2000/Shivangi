2f3f8c6e45c0f9f0da1b241a2a6d4408d261ad8b
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-05-08 19:49:02 +0530

    header

---
+++ src/components/Introduction/Intro.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./Intro.module.css";
+
+export default function Intro() {
+  return (
+    <section className={classes.summary}>
+      <div className={classes.row}>
+        <div className={classes.column}>
+          <img src="shivangi.jpg" />
+        </div>
+        <div className={classes.column}>
+          <h1>Hello!</h1>
+          <h3>A Bit About me</h3>
+          <p>
+            A highly motivated girl, experienced with React JS, JavaScript and
+            GIS Development with Strong technical problem solving and Managing
+            skills, who like to create Impact to the organization / work she is
+            a part of and always love to socialize and experience new things in
+            life.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
