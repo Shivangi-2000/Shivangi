bfdb7cdea65d475f73dd945d810de2b7fb727a79
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-05-09 22:32:16 +0530

    Details

--- src/components/Introduction/Intro.js
+++ src/components/Introduction/Intro.js
@@ -3,23 +3,44 @@ import classes from "./Intro.module.css";
 
 export default function Intro() {
   return (
-    <section className={classes.summary}>
-      <div className={classes.row}>
-        <div className={classes.column}>
-          <img src="shivangi.jpg" />
+    <>
+      <section className={classes.summary}>
+        <div className={classes.row}>
+          <div className={classes.column}>
+            <img src="shivangi.jpg" alt="linkedIn" />
+          </div>
+          <div className={classes.column}>
+            <h1>Hello!</h1>
+            <h3>A Bit About me</h3>
+            <p>
+              A highly motivated girl, experienced with React JS, JavaScript and
+              GIS Development with Strong technical problem solving and Managing
+              skills, who like to create Impact to the organization / work she
+              is a part of and always love to socialize and experience new
+              things in life.
+            </p>
+            <div className="column">
+              <br></br>
+              <div className="row">
+                <div className="column">
+                  <button className=""></button>
+                </div>
+                <div className="column">
+                  <button className=""></button>
+                </div>
+                <div className="column">
+                  <button className=""></button>
+                </div>
+                
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={classes.column}>
-          <h1>Hello!</h1>
-          <h3>A Bit About me</h3>
-          <p>
-            A highly motivated girl, experienced with React JS, JavaScript and
-            GIS Development with Strong technical problem solving and Managing
-            skills, who like to create Impact to the organization / work she is
-            a part of and always love to socialize and experience new things in
-            life.
-          </p>
-        </div>
-      </div>
-    </section>
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </>
   );
 }
