9a91e528f0eb35f8ebce6d0dca03a503984dcc3c
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-05-15 23:38:19 +0530

    loading wheel

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -2,13 +2,13 @@ import React from "react";
 import classes from "./Education.module.css";
 export default function Education(props) {
   return (
-    <section>
+    <section className={classes.education}>
       <br></br><br></br>
-      <div className={classes.row}>
+      <div className={classes.row} key={`Education`}>
         <div className={classes.column}><h2>Education</h2></div>
         <div className={classes.column}>
           {props.degree.map((deg) => (
-            <div className={classes.row}>
+            <div className={classes.row} key={deg.id}>
               <div className={classes.column}>{deg.year}</div>
               <div className={classes.column}><b>{deg.name}</b><br></br>{deg.college}<br></br>{deg.percentage}</div>
             </div>
--- src/components/Header/Headers.js
+++ src/components/Header/Headers.js
@@ -1,47 +1,55 @@
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import "./Headers.css";
+import classes from "./Headers.module.css";
 
 function Headers(props) {
   const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
+    navRef.current.classList.toggle(classes.responsive_nav);
   };
 
   const projectHandler = () => {
     props.onProject(true);
+    showNavbar();
   };
 
   const resumeHandler = () => {
     props.onResume(true);
+    showNavbar();
   };
   const homeHandler = () => {
     props.onHome(true);
+    showNavbar();
   };
 
   return (
-    <header>
-      <div className="logo">
-        <h2 className="name">
-          <span className="sym"> Shivangi Gupta &nbsp;</span>
+    <header >
+      <div className={classes.logo}>
+        <h2 className={classes.name}>
+          <span className={classes.sym}> Shivangi Gupta &nbsp;</span>
         </h2>
         <p>Assistant System Engineer </p>
       </div>
       <nav ref={navRef}>
-        <a href="/#" onClick={homeHandler} >Home</a>
+        <a href="/#" onClick={homeHandler}>
+          Home
+        </a>
         <a href="/#" onClick={resumeHandler}>
           Resume
         </a>
         <a href="/#" onClick={projectHandler}>
           Projects
         </a>
-        
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+
+        <button
+          className={`${classes.navbtn} ${classes.navclosebtn}`}
+          onClick={showNavbar}
+        >
           <FaTimes />
         </button>
       </nav>
-      <button className="nav-btn" onClick={showNavbar}>
+      <button className={classes.navbtn} onClick={showNavbar}>
         <FaBars />
       </button>
     </header>
